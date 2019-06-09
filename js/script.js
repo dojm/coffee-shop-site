71b30456238377c0e0da6da908ce7e715b6206b9
Mary Do <marydo318@gmail.com>
2019-06-09 00:48:18 -0700

    improved hamburger button, animated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const nav = document.querySelector('nav');
+const toggle = document.querySelector('#toggle');
+const toggleFirstLine = document.querySelector('#toggle div:first-child');
+const toggleSecondLine = document.querySelector('#toggle div:nth-child(2)')
+const toggleThirdLine = document.querySelector('#toggle div:last-child');
 
-function closeMenu() {
-  nav.style.width = '0';
-}
+toggle.addEventListener('click', toggleMenu);
 
-function openMenu() {
-  nav.style.width = '80vw';
+function toggleMenu() {
+  let navToggle = nav.classList.toggle('open');
+
+  if (navToggle) {
+    nav.style.width = '80vw';
+    toggleFirstLine.style.transform = 'translateY(9px) rotate(-135deg)';
+    toggleSecondLine.style.opacity = 0;
+    toggleThirdLine.style.transform = 'translateY(-9px) rotate(135deg)'; 
+  } else {
+    nav.style.width = '0px';
+    toggleFirstLine.style.transform = 'translateY(0px) rotate(0deg)';
+    toggleSecondLine.style.opacity = 1;
+    toggleThirdLine.style.transform = 'translateY(0px) rotate(0deg)'; 
+  }
 }
